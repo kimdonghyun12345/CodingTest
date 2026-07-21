e199099f54112fbb1ba164294adbf51d2e69b148
kimdonghyun12345 <gdm200180@gmail.com>
2026-07-21 16:04:19 +0900

    ''

--- src/main/java/kopo/codingtest/level0/subtraction.java
+++ src/main/java/kopo/codingtest/level0/subtraction.java
@@ -9,7 +9,7 @@ public class subtraction {
     }
 
     static int Solution(int num1, int num2) {
-        int answer = num1 - num2;
+        int answer = num1 - num2; 
         return answer;
     }
 }
