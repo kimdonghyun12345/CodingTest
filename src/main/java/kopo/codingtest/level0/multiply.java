6f35dbe5ccdaac41343eb0df948b8852fa713685
kimdonghyun12345 <gdm200180@gmail.com>
2026-07-21 14:12:29 +0900

    Initial commit

---
+++ src/main/java/kopo/codingtest/level0/multiply.java
@@ -0,0 +1,15 @@
+package kopo.codingtest.level0;
+
+public class multiply {
+    public static void main(String[] args) {
+
+        int num1 = 3;
+        int num2 = 4;
+        System.out.println(num1 * num2);
+    }
+
+    static int solution(int num1, int num2) {
+        int answer = num1 * num2;
+        return answer;
+    }
+}
